65c969a4927b85adf4048f341076302a903f16d9
CJEdgerton <cjedgerton@gmail.com>
2017-06-09 21:44:12 -0400

    Updated redirect on login, and welcome page.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,16 +79,12 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Password Safe
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
+                {{-- <div class="links">
                     <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                </div> --}}
             </div>
         </div>
     </body>
